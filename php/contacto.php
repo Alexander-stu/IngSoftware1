4ec01ac12f0510cb0068ec13726791c19aae4d67
Alexander-stu <103070132+Alexander-stu@users.noreply.github.com>
2022-06-08 20:24:58 -0500

    Update contacto.php

--- php/contacto.php
+++ php/contacto.php
@@ -45,7 +45,7 @@
         <nav class="nav-menu" id="mySidenav">
                 <a class="is-hidden-mobile brand is-uppercase has-text-weight-bold has-text-dark" href="../servicios.php">SERVICIOS</a>
                 <ul class="nav-menu-ul">
-                    <li class="nav-menu-item"><a href="contacto.php" class="nav-menu-link">CONTACTANOS</a></li>
+                    <li class="nav-menu-item"><a href="contacto.php" class="nav-menu-link">CONTÁCTANOS</a></li>
                     <li class="nav-menu-item"><a href="nuevacita.php" class="nav-menu-link">NUEVA CITA</a></li>
                     <li class="nav-menu-item"><a href="historialcitas.php" class="nav-menu-link">HISTORIAL DE CITAS</a></li>
                     <li class="nav-menu-item"><a href="../dentistas.php"  class="nav-menu-link">DENTISTAS</a></li>
