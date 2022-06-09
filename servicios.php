810476c34ada2e1f3ea2686d08c192ca47041c09
Alexander-stu <103070132+Alexander-stu@users.noreply.github.com>
2022-06-08 20:31:48 -0500

    Update servicios.php

--- servicios.php
+++ servicios.php
@@ -38,11 +38,11 @@
             <nav class="nav-menu" id="mySidenav">
                 <a class="is-hidden-mobile brand is-uppercase has-text-weight-bold has-text-dark" href="servicios.php">SERVICIOS</a>
                 <ul class="nav-menu-ul">
-                <li class="nav-menu-item"><a href="php/contacto.php" class="nav-menu-link">CONTACTANOS</a></li>
+                <li class="nav-menu-item"><a href="php/contacto.php" class="nav-menu-link">CONTÁCTANOS</a></li>
                     <li class="nav-menu-item"><a href="php/nuevacita.php" class="nav-menu-link">NUEVA CITA</a></li>
                     <li class="nav-menu-item"><a href="php/historialcitas.php" class="nav-menu-link">HISTORIAL DE CITAS</a></li>
                     <li class="nav-menu-item"><a href="dentistas.php"  class="nav-menu-link">DENTISTAS</a></li>
-                    <li class="nav-menu-item"><a href="php/cerrar_sesion.php" class="nav-menu-link">CERRAR SESION</a></li>
+                    <li class="nav-menu-item"><a href="php/cerrar_sesion.php" class="nav-menu-link">CERRAR SESIÓN</a></li>
                 </ul>
             </nav>
         </nav>
